c8705bb40013dc54e58e1d80b3f40a51dc76479b
wielki.borsuk <wielki.borsuk@gmail.com>
2021-01-09 10:25:59 +0100

    Initial commit, spring-boot initializer generated code

---
+++ src/main/java/com/sda/coursemanger/CourseMangerApplication.java
@@ -0,0 +1,13 @@
+package com.sda.coursemanger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseMangerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseMangerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sda/coursemanger/CourseMangerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sda.coursemanger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseMangerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
